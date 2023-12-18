9ee70647f3664a68dbbdec202c7ed0b0ae3976b1
Dorman8288 <dorman8288@gmail.com>
2023-12-18 15:42:42 +0330

    feat: Section 2 Finished

--- P4/Section 2.py
+++ P4/Section 2.py
@@ -11,10 +11,10 @@ def load_images_from_folder(folder):
     images = []
     labels = []
     for filename in os.listdir(folder):
-        label = filename[0]
+        label = int(filename[0])
         img = cv2.imread(os.path.join(folder,filename), cv2.IMREAD_GRAYSCALE)
         if img is not None:
-            images.append(img.flatten())
+            images.append(list(img.flatten()))
             labels.append(label)
     return (images, labels)
 
@@ -24,18 +24,12 @@ trainFolder = "./Data/USPS_images/images/train"
 testData = load_images_from_folder(testFolder)
 trainData = load_images_from_folder(trainFolder)
 
-gamma = 0.5
-C = 10
-
-classifier = svm.SVR(kernel="rbf", gamma=gamma, C=C, cache_size=700)
+classifier = svm.SVC(kernel="rbf", cache_size=700)
 classifier.fit(trainData[0], trainData[1])
 
-print(classifier.score(testData[0], testData[1]))
-
 correct = 0
-print(testData[0])
 y = classifier.predict(testData[0])
-print(y)
 for i in range(len(testData[0])):
     correct += testData[1][i] == y[i]
-print(f"Accuracy: {correct/len(testData)}")
+print(f"Accuracy: {correct/len(testData[0])}")
+
